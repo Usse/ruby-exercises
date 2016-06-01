725ee73ba84173d96437474a38c6ca29ba927781
Andrea Usseglio <darkusse@gmail.com>
2016-06-01 14:46:33 +0100

    the book af ruby :closed_book:

---
+++ theBookOfRuby/ch1.rb
@@ -0,0 +1,9 @@
+
+name = 'Usse'
+greet = "Hello from #{name}"
+puts(greet)
+
+subtotal = 100.00
+taxrate = 0.175
+tax = subtotal * taxrate
+puts "Tax on $#{subtotal} is #{tax}, do grand total is #{subtotal + tax}"
